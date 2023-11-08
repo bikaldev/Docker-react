1c123abcd5afd3d9fa1182ca7d2fe999c6d0fdbd
Bikal Devkota <devkotabikal777@gmail.com>
2023-11-08 14:26:42 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HEY!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React or not!
         </a>
       </header>
     </div>
